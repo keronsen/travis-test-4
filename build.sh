1aa4369f7c74683d6e2f3b8ace3d337753374d23
Karl-Erik Rønsen <keronsen@gmail.com>
2015-03-13 09:55:40 +0100

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,48 @@
+
+set -e
+
+
+## Clean
+
+rm -rf Build
+
+mkdir -p Build/appstax-ios
+
+
+## Build framework
+
+cd Appstax
+xcodebuild build -configuration Release -scheme AppstaxUniversal SYMROOT="../Build/xcodebuild" > ../Build/xcodebuild.log
+cd -
+
+cp -a Build/xcodebuild/Release-universal/Appstax.framework Build/appstax-ios/Appstax.framework
+
+
+## Build examples and starterprojects (verify that they compile OK)
+
+cd Examples/Notes
+xcodebuild build -configuration Release -sdk iphoneos        > /dev/null
+xcodebuild build -configuration Debug   -sdk iphonesimulator > /dev/null
+cd -
+cd StarterProjects/Basic
+xcodebuild build -configuration Release -sdk iphoneos        > /dev/null
+xcodebuild build -configuration Debug   -sdk iphonesimulator > /dev/null
+cd -
+
+
+## Copy to Build/appstax-ios
+
+cp -a StarterProjects Build/appstax-ios/StarterProjects
+cp -a Examples        Build/appstax-ios/Examples
+rm -rf Build/appstax-ios/Examples/Notes/Appstax.framework
+rm -rf Build/appstax-ios/StarterProjects/Basic/Appstax.framework
+cp -a Build/appstax-ios/Appstax.framework Build/appstax-ios/Examples/Notes/Appstax.framework
+cp -a Build/appstax-ios/Appstax.framework Build/appstax-ios/StarterProjects/Basic/Appstax.framework
+
+
+## ZIP
+
+cd Build
+zip -rq appstax-ios.zip appstax-ios
+cd -
+
